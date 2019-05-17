8d743ba47af582ded6b7ef9b5c6dc71bcbc3449f
sam <samuel_schill@gmx.net>
2019-05-17 17:58:57 +0200

    Parser Generator

--- src/edu/hm/schill/samuel/MyParserGenerator.java
+++ src/edu/hm/schill/samuel/MyParserGenerator.java
@@ -1,11 +1,19 @@
 package edu.hm.schill.samuel;
 
+import edu.hm.cs.rs.compiler.lab06rdparsergenerator.RDParserGenerator;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class MyParserGenerator implements edu.hm.cs.rs.compiler.lab06rdparsergenerator.RDParserGenerator {
+public class MyParserGenerator implements RDParserGenerator {
 
+    public static void main(String[] args) {
+        RDParserGenerator pg = new MyParserGenerator();
+
+        String grammar = "=,E=(EOE),E=F,O=+,O=-,F=n,F=-E";
+        System.out.println(pg.generate(grammar));
+    }
     @Override
     public String generate(String grammar) {
         final Map<String[], Set<Character>> firstSets = getFirstSets(grammar);
@@ -18,20 +26,54 @@ public class MyParserGenerator implements edu.hm.cs.rs.compiler.lab06rdparsergen
                 .collect(Collectors.toSet());
 
         return "import java.util.*;\n"
-                + "import java.io.*;\n"
                 + "\n"
                 + "public class RDParser" + start + " {\n"
                 + "    static " + getNodeSourcecode()
                 + "\n"
                 + "    static " + getSyntaxErrorExceptionSourcecode()
                 + "\n"
-                + "    private char lookahead;\n"
+                + "    public static void main(String... args) throws SyntaxErrorException {\n"
+                + "        Node parseTree = new RDParser" + start + "().parse(args[0]);\n"
+                + "        System.out.println(parseTree);  // Parsebaum in einer Zeile\n"
+                + "        parseTree.prettyPrint();        // Parsebaum gekippt, mehrzeilig\n"
+                + "    }"
                 + "\n"
-                + "    public Node parse(String input) throws SyntaxErrorException {\n"
-                + "        lookahead = input.charAt(0);\n"
-                + "        return parse_" + start + "();\n"
+                + "    private char lookahead;\n"
+                + "    private String input;\n"
+                + "\n"
+                + "    public Node parse(String newInput) throws SyntaxErrorException {\n"
+                + "        input = newInput;"
+                + "        return parse" + (int) start + "();\n"
                 + "    }\n"
-                + "\n";
+                + "\n"
+                + tokens.parallelStream().map(token -> {
+            final Set<Character> combinedFirstSet = firstSets.keySet().stream().filter(arr -> arr[0].charAt(0)==token).flatMap(rule -> firstSets.get(rule).stream()).collect(Collectors.toSet());
+
+            return "    private Node parse"+(int) token+"() throws SyntaxErrorException {\n"
+                    + "    lookahead = input.charAt(0);\n"
+                    + "\n"
+                    +
+                    (Character.isUpperCase(token)?
+                            "        if(" + combinedFirstSet.stream().map(chr -> "lookahead != '"+chr+"'").collect(Collectors.joining(" && "))+") \n"
+                                    +		"    	    	throw new SyntaxErrorException(\"Expected one of"
+                                    + combinedFirstSet.stream().map(String::valueOf).collect(Collectors.joining(", "))
+                                    + " but found \" + lookahead);\n"
+                                    +		" 	     Node result = null;\n"
+                                    +		firstSets.keySet().stream().filter(arr -> arr[0].charAt(0)== token).map(rule ->
+                                    "        if("+firstSets.get(rule).stream().map(chr ->
+                                            "lookahead == '"+chr+"'").collect(Collectors.joining(" || ")) + ")\n "
+                                            +		"			result = new Node(\""+token+"\", "+rule[1].chars().mapToObj(chr -> "parse"+chr+"()").collect(Collectors.joining(", "))+");\n"
+                            ).collect(Collectors.joining(""))
+                                    +       "        return result;\n"
+
+                            :
+                            "        if(lookahead != '"+token+"')\n"
+                                    +		"    	        throw new SyntaxErrorException(\"Expected "+token+" but found \"+lookahead);\n\n"
+                                    +       "        input = input.substring(1);\n"
+                                    +		"        return new Node(\""+token+"\");\n")
+                    +		"    }\n";
+        }).collect(Collectors.joining("\n"))
+                + "}\n";
     }
 
     public Map<String[], Set<Character>> getFirstSets (String grammar) {
@@ -49,15 +91,15 @@ public class MyParserGenerator implements edu.hm.cs.rs.compiler.lab06rdparsergen
         while (changed[0]) {
             changed[0] = false;
             for (String[] rule : rules) {
-               char first = rule[1].charAt(0);
-               if (first >= 'A' && first <= 'Z') {
-                   firstSets.keySet().stream()
-                           .filter(otherRule -> otherRule[0].charAt(0) == first)
-                           .forEach(otherRule ->
-                                   changed[0] = firstSets.get(rule).addAll(firstSets.get(otherRule)) || changed[0]);
-               } else {
-                   changed[0] = firstSets.get(rule).add(first) || changed[0];
-               }
+                char first = rule[1].charAt(0);
+                if (first >= 'A' && first <= 'Z') {
+                    firstSets.keySet().stream()
+                            .filter(otherRule -> otherRule[0].charAt(0) == first)
+                            .forEach(otherRule ->
+                                    changed[0] = firstSets.get(rule).addAll(firstSets.get(otherRule)) || changed[0]);
+                } else {
+                    changed[0] = firstSets.get(rule).add(first) || changed[0];
+                }
             }
         }
         return firstSets;
